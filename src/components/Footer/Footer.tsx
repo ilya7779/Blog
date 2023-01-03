35ce98502db4db199f14361abbfeb10c4787ff61
Ilya <ilya.makota01@gmail.com>
2023-01-03 18:04:33 +0400

    feat: created Footer page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,8 @@ import styles from './Footer.module.scss';
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-
+      <div className={styles.footer__blogfolio}>©2022 Blogfolio</div>
+      <div className={styles.footer__rightsReserved}>All rights reserved</div>
     </footer>
   );
 };
